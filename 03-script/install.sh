462b1b87cf6f6bd64f509dd1ae67d5a62b4b710e
Charles Betz <betz4871@stthomas.edu>
2020-08-15 17:56:03 +0000

    more updates

--- 03-script/install.sh
+++ 03-script/install.sh
@@ -4,3 +4,10 @@ set -e  # exit immediately if anything returns non-zero. See https://www.javatpo
 echo "  ----- install node, npm, git -----  "
 apt-get update
 apt-get install -y nodejs npm git
+
+echo "  ----- download, initialize, and run app -----  "
+git clone https://github.com/dm-academy/node-svc-v1
+cd node-svc-v1
+git checkout 02
+npm install
+npm install express 

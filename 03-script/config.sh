94d1be26cc92dfe5cd261c71382575ec82f286f9
Charles Betz <betz4871@stthomas.edu>
2020-08-16 18:54:10 +0000

    many updates to ubuntu version, node, etc

--- 03-script/config.sh
+++ 03-script/config.sh
@@ -5,3 +5,11 @@ echo "  ----- install node, npm, git -----  "
 apt-get update
 apt-get install -y nodejs npm git
 
+# we are also going to install python for using ansible later
+apt-get install -y python
+
+
+# to get latest nodejs do the following
+#npm cache clean -f
+#npm install -g n
+#n stable
--- 03-script/provision.sh
+++ 03-script/provision.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # add new VM
 gcloud compute instances create node-svc \
-    --image-family ubuntu-1604-lts \
+    --image-family ubuntu-minimal-2004-lts \
     --image-project ubuntu-os-cloud \
     --boot-disk-size 10GB \
     --machine-type n1-standard-1
 
 # add firewall rule
-gcloud compute firewall-rules create allow-node-svc--tcp-3000 \
+gcloud compute firewall-rules create allow-node-svc-tcp-3000 \
     --network default \
     --action allow \
     --direction ingress \

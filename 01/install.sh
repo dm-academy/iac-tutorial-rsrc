17063732a4a2c4b9fb3ecaafeeaf7153fbde69b6
Charles Betz <betz4871@stthomas.edu>
2020-09-05 19:36:28 +0000

    updates

--- 01/install.sh
+++ 01/install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e  # exit immediately if anything returns non-zero. See https://www.javatpoint.com/linux-set-command
 
-echo "  ----- download, initialize, and run app -----  "
-git clone https://github.com/dm-academy/node-svc-v1
-cd node-svc-v1
-git checkout 02a
+echo "  ----- download and initialize app -----  "
+git clone https://github.com/dm-academy/node-svc
+cd node-svc
+git checkout 02
 
 npm install express --save
 npm install node-fetch --save

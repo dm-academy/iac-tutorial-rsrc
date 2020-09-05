044a9fc560c79c34d5bae854538290b7f3470483
Charles Betz <betz4871@stthomas.edu>
2020-09-05 18:05:53 +0000

    restructure

--- README.md
+++
@@ -1,3 +0,0 @@
-## About the repo
-
-This repository is used as a part of [infrastructure-as-code-tutorial](https://github.com/dm-academy/infrastructure-as-code-tutorial).
--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker run --name raddit-app \
-    --env DATABASE_HOST=mongo-database \
-    --network raddit-network \
-    --publish 9292:9292 \
-    --detach raddit
